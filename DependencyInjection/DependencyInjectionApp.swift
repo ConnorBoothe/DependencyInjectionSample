3006a5daccf0c1568b724693ae3a1304faeb6ddb
Connor Boothe <cboothe@ccgiq.com>
2023-06-21 10:24:54 -0400

    Initial Commit

---
+++ DependencyInjection/DependencyInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependencyInjectionApp.swift
+//  DependencyInjection
+//
+//  Created by Connor Boothe on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct DependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
